958e4a61868656bc8b2dadbb9fa7a5355073e833
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2023-12-21 19:28:39 -0800

    second function: RED

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -8,4 +8,15 @@ describe('AgeCalculator', () => {
         expect(ageToConvert.calculate('Mars')).toEqual(21.27);
         expect(ageToConvert.calculate('Jupiter')).toEqual(3.37);
     });
+    test('should create a map with keys as names of 4 planets and values as years of ages convered from earth current age and past age difference', () => {
+        const earthAge = new AgeCalculator(30);
+        expect(earthAge.yearsSincePast(10)).toBe(
+            {
+                Mercury: 83.33,
+                Venus: 32.25,
+                Mars: 10.63,
+                Jupiter: 1.68
+            }
+        )
+    })
 })
