2fb07b7d418a1b6a09eb4bc5175081280ed7fd68
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2023-12-21 19:11:26 -0800

    combined all tests into 1 for the first function: GREEN

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,20 +1,11 @@
 import AgeCalculator from "../src/js/calculator";
 
 describe('AgeCalculator', () => {
-    test('should correctly calculate current age in Mercury to fixed 2 decimals', () => {
-        const ageInMercury = new AgeCalculator(40);
-        expect(ageInMercury.calculate('Mercury')).toEqual(166.66);
-    });
-    test('should correctly calculate current age in Venus to fixed 2 decimals', () => {
-        const ageInVenus = new AgeCalculator(40);
-        expect(ageInVenus.calculate('Venus')).toEqual(64.51);
-    });
-    test('should correctly calculate current age in Mars to fixed 2 decimals', () => {
-        const ageInMars = new AgeCalculator(40);
-        expect(ageInMars.calculate('Mars')).toEqual(21.27);
-    });
-    test('should correctly calculate current age in Jupiter to fixed 2 decimals', () => {
-        const ageInJupiter = new AgeCalculator(40);
-        expect(ageInJupiter.calculate('Jupiter')).toEqual(3.37);
+    test('should correctly calculate current age to ages in 4 planets in 2 fixed decimals', () => {
+        const ageToConvert = new AgeCalculator(40);
+        expect(ageToConvert.calculate('Mercury')).toEqual(166.66);
+        expect(ageToConvert.calculate('Venus')).toEqual(64.51);
+        expect(ageToConvert.calculate('Mars')).toEqual(21.27);
+        expect(ageToConvert.calculate('Jupiter')).toEqual(3.37);
     });
 })
