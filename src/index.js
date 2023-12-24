427a59552a19bc1ec6c98561887df1706aa5dc8f
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2023-12-23 23:37:27 -0800

    simplifying UI logic

--- src/index.js
+++ src/index.js
@@ -1,67 +1,63 @@
 import './css/styles.scss';
 import AgeCalculator from './js/calculator';
 
-function ages() {
-    const current = document.getElementById("currentAge").value;
-    const past = document.getElementById("pastAge").value;
-    const future = document.getElementById("futureAge").value;
-    const ageMap = new Map();
-    ageMap.set("current", current);
-    ageMap.set("past", past);
-    ageMap.set("future", future);
-    return ageMap;
+function getAge(id) {
+    return document.getElementById(id).value;
+}
+function updatePlanetInfo(current, since, until, ageCalculator, planet) {
+    current.innerText = ageCalculator.calculate(planet);
+    since.innerText = ageCalculator.yearsSincePast()[planet];
+    until.innerText = ageCalculator.yearsUntilFuture()[planet];
 }
 function formHandler() {
     const form = document.querySelector("form");
-    
-    const mercuryNow = document.getElementById("mercury-now");
-    const mercurySince = document.getElementById("mercury-since");
-    const mercuryUntil = document.getElementById("mercury-until");
-    
-    const venusNow = document.getElementById("venus-now");
-    const venusSince = document.getElementById("venus-since");
-    const venusUntil = document.getElementById("venus-until");
 
-    const marsNow = document.getElementById("mars-now");
-    const marsSince = document.getElementById("mars-since");
-    const marsUntil = document.getElementById("mars-until");
-
-    const jupiterNow = document.getElementById("jupiter-now");
-    const jupiterSince = document.getElementById("jupiter-since");
-    const jupiterUntil = document.getElementById("jupiter-until");
+    const planetElements = {
+        Mercury: {
+            now: document.getElementById("mercury-now"),
+            since: document.getElementById("mercury-since"),
+            until:document.getElementById("mercury-until")
+        },
+        Venus: {
+            now: document.getElementById("venus-now"),
+            since: document.getElementById("venus-since"),
+            until: document.getElementById("venus-until")
+        },
+        Mars: {
+            now: document.getElementById("mars-now"),
+            since: document.getElementById("mars-since"),
+            until: document.getElementById("mars-until")
+        },
+        Jupiter: {
+            now: document.getElementById("jupiter-now"),
+            since: document.getElementById("jupiter-since"),
+            until: document.getElementById("jupiter-until")
+        }
+    };
 
     const explanation = document.getElementById("explanation");
     
     form.addEventListener("submit", e => {
         e.preventDefault();
-        const ageMap = ages();
-        const currentAge = ageMap.get("current");
-        const pastAge = ageMap.get("past");
-        const futureAge = ageMap.get("future");
-        const newAge = new AgeCalculator(currentAge);
-        const pastAgeMap = newAge.yearsSincePast(pastAge);
-        const futureAgeMap = newAge.yearsUntilFuture(futureAge);
-        
-        mercuryNow.innerText = newAge.calculate('Mercury');
-        mercurySince.innerText = pastAgeMap['Mercury'];
-        mercuryUntil.innerText = futureAgeMap['Mercury'];
-        
-        venusNow.innerText = newAge.calculate('Venus');
-        venusSince.innerText = pastAgeMap['Venus'];
-        venusUntil.innerText = futureAgeMap['Venus'];
 
-        marsNow.innerText = newAge.calculate('Mars');
-        marsSince.innerText = pastAgeMap['Mars'];
-        marsUntil.innerText = futureAgeMap['Mars'];
+        const currentAge = getAge("currentAge");
+        const pastAge = getAge("pastAge");
+        const futureAge = getAge("futureAge");
 
-        jupiterNow.innerText = newAge.calculate('Jupiter');
-        jupiterSince.innerText = pastAgeMap["Jupiter"];
-        jupiterUntil.innerText = futureAgeMap["Jupiter"];
+        const ageCalculator = new AgeCalculator(currentAge, pastAge, futureAge);
+
+        for (const planet in planetElements) {
+            updatePlanetInfo(
+                planetElements[planet].now,
+                planetElements[planet].since,
+                planetElements[planet].until,
+                ageCalculator,
+                planet
+            );
+        }
 
         explanation.classList.remove("hidden");
     });
 }
 
-window.onload = () => {
-    formHandler();
-};
+window.onload = formHandler;
